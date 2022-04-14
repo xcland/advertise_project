8a702516c6cceb22224f9fa9b9905894fafd5028
XCL <xclancn@outlook.com>
2022-04-14 22:23:13 +0800

    complete Account component of searchPromotion page

--- src/pages/searchPromotion/components/Account/index.tsx
+++ src/pages/searchPromotion/components/Account/index.tsx
@@ -1,12 +1,48 @@
 import React from 'react'
+import { StarOutlined, StarFilled, StarTwoTone } from '@ant-design/icons'
 import './style.scss'
+import { Button } from 'antd'
 
 interface IProps {}
 interface IStates {}
 
 class Account extends React.Component<IProps, IStates> {
   render(): React.ReactNode {
-    return <div>Account</div>
+    return (
+      <div className="search-promotion-account-component-box">
+        <div className="name">wdnmd</div>
+        <div className="info">
+          <div className="info-item">
+            <span className="title-text">客户权益</span>
+            <span>
+              <StarFilled />
+              <StarFilled />
+              <StarOutlined />
+              <StarOutlined />
+              <StarOutlined />
+            </span>
+          </div>
+          <div className="info-item">
+            <span className="title-text">状态</span>
+            <span className="circle"></span>
+            <span className="value-text">开户金未到</span>
+          </div>
+          <div className="info-item">
+            <span className="title-text">推广余额</span>
+            <span className="value-text">0</span>
+            <Button size="small">充值</Button>
+          </div>
+          <div className="info-item">
+            <span className="title-text">预算</span>
+            <span className="value-text">不限定预算</span>
+          </div>
+          <div className="info-item">
+            <span className="title-text">地域</span>
+            <span className="value-text">不限定地域</span>
+          </div>
+        </div>
+      </div>
+    )
   }
 }
 
